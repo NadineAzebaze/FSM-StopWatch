1867c3bdce61a2f77c3c1e0fc074c10b19ef5761
NadineAzebaze <nadine-carel.azebaze-zewo@etu.univ-cotedazur.fr>
2021-10-02 12:24:42 +0200

    version 3

--- StopWatchV0/fr.univcotedazur.polytech.si4.fsm.stopwatch.v0/src/fr/univcotedazur/polytech/si4/fsm/stopwatch/v0/StopWatchGUI.java
+++ StopWatchV0/fr.univcotedazur.polytech.si4.fsm.stopwatch.v0/src/fr/univcotedazur/polytech/si4/fsm/stopwatch/v0/StopWatchGUI.java
@@ -11,34 +11,32 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-import com.yakindu.core.TimerService;
-
-import fr.unice.polytech.si4.StopWatchStateMachine;
-
-
-
 
 /**
  * Simple old style GUI for the stopWatch used as a support for the {@see <a href="http://www.i3s.unice.fr/~deantoni/teaching_resources/SI4/FSM/">Finite State Machine course</a>}
  * @author Julien Deantoni, universite cote d'azur
  *
  */
-public class StopWatchGUI extends JFrame  {
+public class StopWatchGUI extends JFrame {
 
+	
+	/**
+	 * enum
+	 */
+	enum States{ INIT, START, STOP, RESET, PAUSE, RESUME}
+	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8682173885223592966L;
 
-	protected int millis, secs, mins;
-	protected JButton leftButton, rightButton, modeButton;
+	protected int millis, secs, mins, counter;
+	//protected boolean pause, start;
+	protected JButton leftButton, rightButton;
 	protected JPanel rootPanel;
-	protected JLabel timeValue, modeValue;
+	protected JLabel timeValue;
 	protected Timer msTimer;
-	protected StopWatchStateMachine theFSM;
-	
-	
-	
+	protected States state;
 
 	
 	/**
@@ -65,54 +63,38 @@ public class StopWatchGUI extends JFrame  {
 				+ (((millis / 10) == 0) ? "00" : (((millis / 100) == 0) ? "0" : "")) + millis);
 		repaint();
 	}
-	
-	public void count() {
-		// TODO Auto-generated method stub
-		count(7);
-		//updateTimeValue();
-		
-	}
-	protected void doPrintDate() {
-		modeButton.setText("time");
-		modeValue.setText(java.time.LocalDate.now().toString());
-		//System.out.println(java.time.LocalDate.now());
-	}
 
-	protected void doPrintHour() {
-		modeButton.setText("date");
-		modeValue.setText(java.time.LocalTime.now().toString());
-		//System.out.println(java.time.LocalTime.now());
-	}
 	protected void doStart() {
-		//msTimer.start();
-		//updateTimeValue();
+		this.state = States.START;
+		msTimer.start();
+		updateTimeValue();
 		leftButton.setText("stop");
+		counter++;
+		//start = true;
 	}
 
-	protected void doStop() {
-		//msTimer.stop();
-		//updateTimeValue();
-		leftButton.setText("reset");
-	}
-
+////modif
 	protected void doPause() {
-		//msTimer.start();
-		//updateTimeValue();
+		this.state = States.PAUSE;
 		rightButton.setText("resume");
+		//pause = true;
 	}
-
+	
 	protected void doResume() {
-		//msTimer.stop();
-		//updateTimeValue();
+		this.state = States.RESUME;
 		rightButton.setText("pause");
 	}
 	
-	protected void doReset() {
-		//msTimer.start();
-		//updateTimeValue();
-		mins=millis=secs=0;
+	
+	protected void doStop() {
+		this.state = States.STOP;
 		leftButton.setText("start");
+		msTimer.stop();
+		updateTimeValue();
+		leftButton.setText("start");
+		counter++;
 	}
+
 	
 	/**
 	 * construct the GUI and initialize the different value. Also initialize the {@link #msTimer}
@@ -120,52 +102,39 @@ public class StopWatchGUI extends JFrame  {
 	 * @param se
 	 * @param ct
 	 */
-	public StopWatchGUI(int mn, int se, int ct) {
+	public StopWatchGUI(int mn, int se, int ct, int count) {
 		mins = mn;
 		secs = se;
 		millis = ct;
-		theFSM = new StopWatchStateMachine(); 
-
-		TimerService timer = new TimerService();
-		theFSM.setTimerService(timer);
-		
-		theFSM.getDoStart().subscribe(new MyObserverStart(this));
-		theFSM.getDoStop().subscribe(new MyObserverStop(this));
-		theFSM.getDoPause().subscribe(new MyObserverPause(this));
-		theFSM.getDoResume().subscribe(new MyObserverResume(this));
-		theFSM.getDoReset().subscribe(new MyObserverReset(this));
-		theFSM.getPrintDate().subscribe(new MyObserverPrintDate(this));
-		theFSM.getPrintHour().subscribe(new  MyObserverPrintHour(this));
-		theFSM.getCount().subscribe(new MyObserverCount(this));
-		
-		theFSM.enter();
-
-
+		counter = count;
 		// graphics init and listener settings
 		rootPanel = new JPanel();
 		leftButton = new JButton("start");
 		leftButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				theFSM.raiseLeftButton();
-
+				switch(state) {
+				case START:
+					doStart();
+					break;
+				case STOP:
+					doStop();
+				}
 			}
 		});
-
+	
 		rightButton = new JButton("pause");
 		rightButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				theFSM.raiseRightButton();
-				
-			}
-		});
+				switch(state) {
+				case PAUSE:
+					doPause();
+					break;
+				case RESUME:
+					doResume();
+				}
 		
-		modeButton = new JButton("time");
-		modeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				theFSM.raiseModeButton();
 			}
 		});
 
@@ -173,16 +142,10 @@ public class StopWatchGUI extends JFrame  {
 		timeValue.setFont(new Font("Courier", Font.BOLD, 25));
 		updateTimeValue();
 		rootPanel.add(timeValue);
-		
-		modeValue = new JLabel();
-		modeValue.setFont(new Font("Courier", Font.BOLD, 25));
-		//updateTimeValue();
-		rootPanel.add(modeValue);
 
 		JPanel buttonPanel = new JPanel(new FlowLayout());
 		buttonPanel.add(leftButton);
 		buttonPanel.add(rightButton);
-		buttonPanel.add(modeButton);
 		rootPanel.add(buttonPanel);
 		this.add(rootPanel);
 
@@ -190,14 +153,22 @@ public class StopWatchGUI extends JFrame  {
 		ActionListener doCountEvery7 = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				count(7);
+				switch(state) {
+				case START, RESTART, RESUME:
+					count(7);
+					updateTimeValue();
+					break;
+				case PAUSE:
+					count(7);
+				}
 			}
 		};
+		
 		msTimer = new Timer(7, doCountEvery7);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().add(rootPanel);
-		setSize(230, 150);
+		setSize(200, 110);
 		setResizable(true);
 		setTitle("stopwatch");
 		setVisible(true);
@@ -205,10 +176,6 @@ public class StopWatchGUI extends JFrame  {
 	}
 
 	public static void main(String args[]) {
-		new StopWatchGUI(0, 0, 0);
+		new StopWatchGUI(0, 0, 0, 0);
 	}
-
-	
-
-
 }
